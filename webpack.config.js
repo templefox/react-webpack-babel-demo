e836e370e02cd1d1153efb94d6f4e8bd9a3c7c16
unknown <I302583@PVGN50863253A.apj.global.corp.sap>
2015-12-31 16:54:08 +0800

    hello world

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+
+module.exports = {
+	context: __dirname + "/app",
+	entry: {
+		javascript: "./app.js",
+		html: "./index.html"
+	},
+
+	output: {
+		filename: "app.js",
+		path: __dirname + "/dist",
+	},
+
+	module: {
+		loaders: [
+		{
+			test: /\.jsx?$/,
+			exclude: /node_modules/,
+			loader: 'babel-loader',
+			query:
+			{
+				presets:['react','es2015']
+			}
+		},
+		{
+			test: /\.html$/,
+			loader: "file?name=[name].[ext]",
+		},
+		],
+	},
+}
